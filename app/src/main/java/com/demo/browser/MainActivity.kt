fb68f48267068defc66e3ea29a5cccc26c8b7227
Mohammed tag <mohamed.taj@thed.io>
2019-09-03 02:09:03 +0200

    init dagger , creating webview

--- app/src/main/java/com/demo/browser/MainActivity.kt
+++ app/src/main/java/com/demo/browser/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.demo.browser
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.demo.browser.app.App
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+
         (application as App).compnant.inject(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://google.com")
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient= object : WebChromeClient(){
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+            }
+        }
     }
 }
