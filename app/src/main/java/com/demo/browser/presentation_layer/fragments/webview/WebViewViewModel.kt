57b704e9a967a681a2fda02f0e54d31df5a12dbd
Mohammed tag <mohamed.taj@thed.io>
2019-09-05 00:59:18 +0200

    creating layers of the clean arch. and the use cases

--- app/src/main/java/com/demo/browser/presentation_layer/fragments/webview/WebViewViewModel.kt
+++ app/src/main/java/com/demo/browser/presentation_layer/fragments/webview/WebViewViewModel.kt
@@ -1,10 +1,12 @@
 package com.demo.browser.presentation_layer.fragments.webview
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.demo.browser.data_layer.models.SuccessfulUrl
 import com.demo.browser.domain_layer.usecases.add_url_use_case.AddUrlsListUseCase
+import com.demo.browser.domain_layer.usecases.retrieve_urls_use_case.RetrieveUrlsUseCase
 import com.demo.browser.domain_layer.usecases.url_use_case.UrlUseCase
 import dagger.Module
 import dagger.Provides
@@ -17,20 +19,29 @@ import javax.inject.Singleton
  */
 
 
-class WebViewViewModel(private val urlUserCase: UrlUseCase, private val addUrlsListUseCase: AddUrlsListUseCase) :
+class WebViewViewModel(
+    private val urlUserCase: UrlUseCase,
+    private val addUrlsListUseCase: AddUrlsListUseCase,
+    private val retrieveUrlsUseCase: RetrieveUrlsUseCase
+) :
     ViewModel() {
 
 
     fun urlHandel(url: String): MutableLiveData<String> = urlUserCase.run(url)
     fun saveSuccessfulUrlList(successfulUrlsList: ArrayList<SuccessfulUrl>) = addUrlsListUseCase.run(successfulUrlsList)
+    fun retrieveUrlsList() : LiveData<List<SuccessfulUrl>> = retrieveUrlsUseCase.run()
 
 }
 
-class WebViewViewModelFactory(private val urlUserCase: UrlUseCase,private val addUrlsListUseCase: AddUrlsListUseCase) : ViewModelProvider.Factory {
+class WebViewViewModelFactory(
+    private val urlUserCase: UrlUseCase,
+    private val addUrlsListUseCase: AddUrlsListUseCase,
+    private val retrieveUrlsUseCase: RetrieveUrlsUseCase
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         if (modelClass.isAssignableFrom(WebViewViewModel::class.java))
-            return WebViewViewModel(urlUserCase,addUrlsListUseCase) as T
+            return WebViewViewModel(urlUserCase, addUrlsListUseCase, retrieveUrlsUseCase) as T
 
         throw IllegalArgumentException("Unknown ViewModel class")
     }
@@ -41,6 +52,10 @@ class WebViewViewModelFactoryModule {
     @Provides
     @Singleton
 
-    fun ProvidesWebViewViewModelFactory(urlUserCase: UrlUseCase,addUrlsListUseCase: AddUrlsListUseCase): WebViewViewModelFactory =
-        WebViewViewModelFactory(urlUserCase,addUrlsListUseCase)
+    fun ProvidesWebViewViewModelFactory(
+        urlUserCase: UrlUseCase,
+        addUrlsListUseCase: AddUrlsListUseCase,
+        retrieveUrlsUseCase: RetrieveUrlsUseCase
+    ): WebViewViewModelFactory =
+        WebViewViewModelFactory(urlUserCase, addUrlsListUseCase, retrieveUrlsUseCase)
 }
